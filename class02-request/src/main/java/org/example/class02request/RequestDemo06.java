34d58a2de86ac493726e5139cde8226d1269daf4
linxia74 <2316437863@qq.com>
2024-09-22 00:16:05 +0800

    java-learning>

--- class02-request/src/main/java/org/example/class02request/RequestDemo06.java
+++ class02-request/src/main/java/org/example/class02request/RequestDemo06.java
@@ -1,4 +1,21 @@
 package org.example.class02request;
 
-public class RequestDemo06 {
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/requestDemo06")
+public class RequestDemo06 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("RequestDemo06被访问......");
+
+        ServletContext servletContext = req.getServletContext();
+        servletContext.setAttribute("info","message");
+    }
 }
--- class02-request/src/main/java/org/example/class02request/RequestDemo07.java
+++ class02-request/src/main/java/org/example/class02request/RequestDemo07.java
@@ -1,4 +1,20 @@
 package org.example.class02request;
 
-public class RequestDemo07 {
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/requestDemo07")
+public class RequestDemo07 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = req.getServletContext();
+        String info = String.valueOf(servletContext.getAttribute("info"));
+        System.out.println(info);
+    }
 }
