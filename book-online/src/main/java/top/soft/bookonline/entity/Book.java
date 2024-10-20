1f01d4669a49077c4858e0eb75291a05ff39186a
linxia74 <2316437863@qq.com>
2024-10-20 13:28:02 +0800

    Merge remote-tracking branch 'origin/main'

---
+++ book-online/src/main/java/top/soft/bookonline/entity/Book.java
@@ -0,0 +1,22 @@
+package top.soft.bookonline.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 23164
+ * @description: TODO
+ * @date 2024/10/19 16:28
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Book {
+    private Integer id;
+    private String name;
+    private String cover;
+    private String author;
+}
